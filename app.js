94b37c09f98a791353a45a31bebc3a2524c3e3b6
Gary Pang <gary.pang@gmail.com>
2016-01-14 00:58:30 -0500

    Update index.html
    Webpage now shows error messages only after user submits form.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ validationApp.controller('mainController', function ($scope) {
     // Function to submit form after all validation has occurred
     $scope.submitForm = function (isValid) {
 
+        $scope.submitted = true;
+
         // Check whether whole form is valid
         if (isValid) {
             alert('This form is awesome');
